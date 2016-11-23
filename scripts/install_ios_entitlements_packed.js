8cce77c02fcf9015322b26063dbb83bbd4d34f8c
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-11-23 17:40:40 +0200

    Disable alternations on the native project

--- scripts/install_ios_entitlements_packed.js
+++ scripts/install_ios_entitlements_packed.js
@@ -3710,57 +3710,57 @@ var xcode = __webpack_require__(/*! xcode */ 5),
     projFolder,
     projName;
 
-// Find the project folder by looking for *.xcodeproj
-if (!(data && data.length)) {
-  console.log("platforms/ios does not exist, skip copying entitlements.");
-} else {
-  data.forEach(function (folder) {
-    if (folder.match(/\.xcodeproj$/)) {
-      projFolder = path.join(iosFolder, folder);
-      projName = path.basename(folder, '.xcodeproj');
-    }
-  });
+// // Find the project folder by looking for *.xcodeproj
+// if (!(data && data.length)) {
+//   console.log("platforms/ios does not exist, skip copying entitlements.");
+// } else {
+//   data.forEach(function (folder) {
+//     if (folder.match(/\.xcodeproj$/)) {
+//       projFolder = path.join(iosFolder, folder);
+//       projName = path.basename(folder, '.xcodeproj');
+//     }
+//   });
 
-  if (!projFolder || !projName) {
-    throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
-  }
+//   if (!projFolder || !projName) {
+//     throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
+//   }
 
-  var destFolder = path.join(iosFolder, projName, 'Resources');
-  if (!fs.existsSync(destFolder)) {
-    fs.mkdirSync(destFolder);
-  }
+//   var destFolder = path.join(iosFolder, projName, 'Resources');
+//   if (!fs.existsSync(destFolder)) {
+//     fs.mkdirSync(destFolder);
+//   }
 
-  var destFile = path.join(destFolder, projName + '.entitlements');
+//   var destFile = path.join(destFolder, projName + '.entitlements');
 
-  if (!fs.existsSync(destFile)) {
-    var bundleID = pjson.nativescript.id;
+//   if (!fs.existsSync(destFile)) {
+//     var bundleID = pjson.nativescript.id;
 
-    // create a new entitlements plist file
-    var sourceFile = path.join('node_modules', 'nativescript-plugin-firebase', 'scripts', 'resources', 'KeychainSharing.entitlements');
+//     // create a new entitlements plist file
+//     var sourceFile = path.join('node_modules', 'nativescript-plugin-firebase', 'scripts', 'resources', 'KeychainSharing.entitlements');
 
-    var fileData = fs.readFileSync(sourceFile).toString();
-    fileData = fileData.replace(/__KEYCHAIN_ACCESS_GROUP__/g, bundleID);
-    fs.writeFileSync(destFile, fileData);
+//     var fileData = fs.readFileSync(sourceFile).toString();
+//     fileData = fileData.replace(/__KEYCHAIN_ACCESS_GROUP__/g, bundleID);
+//     fs.writeFileSync(destFile, fileData);
 
-    var projectPath = path.join(projFolder, 'project.pbxproj'),
-        pbxProject = xcode.project(projectPath);
+//     var projectPath = path.join(projFolder, 'project.pbxproj'),
+//         pbxProject = xcode.project(projectPath);
 
-    pbxProject.parseSync();
-    pbxProject.addResourceFile(path.join(projName, "Resources", projName + ".entitlements"));
+//     pbxProject.parseSync();
+//     pbxProject.addResourceFile(path.join(projName, "Resources", projName + ".entitlements"));
 
 
-    var configGroups = pbxProject.hash.project.objects['XCBuildConfiguration'];
-    for (var key in configGroups) {
-      var config = configGroups[key];
-      if (config.buildSettings !== undefined) {
-        config.buildSettings.CODE_SIGN_ENTITLEMENTS = '"' + projName + '/Resources/' + projName + '.entitlements"';
-      }
-    }
+//     var configGroups = pbxProject.hash.project.objects['XCBuildConfiguration'];
+//     for (var key in configGroups) {
+//       var config = configGroups[key];
+//       if (config.buildSettings !== undefined) {
+//         config.buildSettings.CODE_SIGN_ENTITLEMENTS = '"' + projName + '/Resources/' + projName + '.entitlements"';
+//       }
+//     }
 
-    // write the updated project file
-    fs.writeFileSync(projectPath, pbxProject.writeSync());
-  }
-}
+//     // write the updated project file
+//     fs.writeFileSync(projectPath, pbxProject.writeSync());
+//   }
+// }
 
 
 /***/ }
